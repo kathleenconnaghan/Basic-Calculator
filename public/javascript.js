05666dd0c7b5a5c2323acf9363a89643e648aac2
kathleenconnaghan <kathleen.connaghan@gmail.com>
2021-03-06 19:25:30 -0500

    first commit

---
+++ public/javascript.js
@@ -0,0 +1,15 @@
+function clk(val){
+
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+
+function clrdisp(){
+
+    document.getElementById("screen").value = ""
+}
+
+function eql (){
+    let text=document.getElementById("screen").value;
+    let result = eval(text);
+    document.getElementById("screen").value=result;
+}
